84db2d97539eca889a1f166c7ba373baf26aa5bc
whitekotan <gcat5770@gmail.com>
2026-05-22 14:33:48 +0300

    feat: create main view

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,8 @@
+from django.db.models import Count
+from django.shortcuts import render
+from .models import Recipe, Category
+
+def main(request):
+    recipes = Recipe.objects.order_by('-created_at')[:5]
+    return render(request, 'main.html', {'recipes': recipes})
 
