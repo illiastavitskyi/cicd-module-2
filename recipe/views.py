fe887e018c01eff7d043e387cced1d20069a31e2
whitekotan <gcat5770@gmail.com>
2026-05-22 14:36:01 +0300

    feat: create view for category list

--- recipe/views.py
+++ recipe/views.py
@@ -6,3 +6,6 @@ def main(request):
     recipes = Recipe.objects.order_by('-created_at')[:5]
     return render(request, 'main.html', {'recipes': recipes})
 
+def category_list(request):
+    categories = Category.objects.annotate(recipe_count=Count('categories'))
+    return render(request, 'category_list.html', {'categories': categories})
